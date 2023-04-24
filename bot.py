6446be575889f846b0bfbfe36b4467ba61a62e80
Willian Josue <48298072+wjoe1995@users.noreply.github.com>
2023-04-24 13:31:39 -0600

    cambios en el menu

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import subprocess
 
 load_dotenv()
 
-
+#1271515359
 BOT_TOKEN = os.environ.get('TELEGRAM_API_KEY')
 
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -22,25 +22,60 @@ def menu_interactivo(chat_id, opciones):
 def menu(message):
     opciones = {
         "start": "/start",
-        "aulas": "/aulas",
-        "usuarios": "/usuarios"
+        #"aulas": "/aulas",
+        #"usuarios": "/usuarios",
+        "Solicitud de Registro de Estudiantes": "/solicitudRegistroEstudiantes",
+        "Tutorias": {
+            "Ver mi historial de tutorias recibidas": "/verHistorialTutoriasRecibidas",
+            "Ver tutorias activas": "/verTutoriasActivas",
+            "Solicitar tutoria": {
+                "Formulario de solicitud": "/solicitarTutoria",
+                "Ver estado de solicitud": "/verEstadoSolicitudTutoria",
+                "Ver historial tutorias recibidas": "/verHistorialTutoriasRecibidas",
+                "Ver tutorias activas": "/verTutoriasActivas",
+                "Regresar": "back"
+            },
+            "Solicitar ser tutor": {
+                "Formulario de solicitud": "/solicitarSerTutor",
+                "Ver estado de solicitud": "/verEstadoSolicitudSerTutor",
+                "Ver Tutorias Impartidas": "/verTutoriasImpartidas",
+                "Ver Tutorias Activas": "/verTutoriasActivas",
+                "Regresar": "back"
+            },
+            "Regresar": "back"
+        }
+        
     }
     menu_interactivo(message.chat.id, opciones.keys())
+    print(message.chat.id)
     bot.register_next_step_handler(message, lambda m: seleccionar_opcion(m, opciones))
 
-    def seleccionar_opcion(message, opciones):
+    def seleccionar_opcion(message, opciones, historial=[]):
         opcion_seleccionada = message.text
         if opcion_seleccionada in opciones:
             comando = opciones[opcion_seleccionada]
-            mensaje_comando = f"Ejecuta el comando {comando}"
-            bot.send_message(message.chat.id, mensaje_comando)
-            try:
-                #ejecutar el comando /start
-                bot.send_message(message.chat.id, comando)
-            except Exception as e:
-                bot.send_message(message.chat.id, f"Error al ejecutar el comando: {str(e)}")
+            if isinstance(comando, dict):  # Si la opción seleccionada es un diccionario, mostrar un submenú
+                historial.append(opciones)  # Agregar el menú actual al historial
+                menu_interactivo(message.chat.id, comando.keys())
+                bot.register_next_step_handler(message, lambda m: seleccionar_opcion(m, comando, historial))
+            elif comando == "back":  # Si la opción seleccionada es "Atrás", retroceder en el historial
+                if historial:
+                    menu_anterior = historial.pop()
+                    menu_interactivo(message.chat.id, menu_anterior.keys())
+                    bot.register_next_step_handler(message, lambda m: seleccionar_opcion(m, menu_anterior, historial))
+                else:
+                    bot.send_message(message.chat.id, "No hay menú anterior")
+            else:
+                mensaje_comando = f"Ejecuta el comando {comando}"
+                bot.send_message(message.chat.id, mensaje_comando)
+                try:
+                    bot.send_message(message.chat.id, comando)
+                except Exception as e:
+                    bot.send_message(message.chat.id, f"Error al ejecutar el comando: {str(e)}")
         else:
             bot.send_message(message.chat.id, "Opción inválida")
+
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
